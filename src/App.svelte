a00bb51d7e3c54a05611c98de4f6ef6cf6be0351
netapy <39877685+netapy@users.noreply.github.com>
2022-10-15 13:45:55 +0200

    initial

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+  <body style="overflow-x: hidden;">
+    <div >
+      <div class="d-flex align-items-center justify-content-center">
+        <img class="landingImage" src="assets/images/landing1.png" alt="tabouret ploia">
+        <div class="text-center p-3" style="position:absolute; width:100%; bottom:0px">
+          <h1>PLOIA</h1>
+        </div>
+      </div>
+    </div>
+  </body>
+</main>
+
+<style>
+  .landingImage{
+    height: 100vh;
+  }
+</style>
